ea681564e4ca389e5ca0391d23871c20c676e5ce
Ketan Sethi <ketansethi786@gmail.com>
2020-09-02 19:46:19 +0530

    Adding POJO classes to deserialize the response

---
+++ src/main/java/com/ndtv/social/pojo/Wind.java
@@ -0,0 +1,29 @@
+package com.ndtv.social.pojo;
+
+public class Wind {
+    private String deg;
+
+    private String speed;
+
+    public String getDeg() {
+        return deg;
+    }
+
+    public void setDeg(String deg) {
+        this.deg = deg;
+    }
+
+    public String getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(String speed) {
+        this.speed = speed;
+    }
+
+    @Override
+    public String toString() {
+        return "ClassPojo [deg = " + deg + ", speed = " + speed + "]";
+    }
+}
+
